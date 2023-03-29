83b8757e8ccd6a4f8e7386329991bfb7b2676eb5
Aleksandar <coa@hotmail.com>
2023-03-29 08:06:15 +0200

    upload

--- trakt/util.py
+++ trakt/util.py
@@ -9,13 +9,34 @@ def get_user_tokens(session_id):
         return user_tokens[0]
     return None
 
+def refresh_spotify_token(session_id):
+
+    refresh_token = TraktToken.objects.filter(session_id).refresh_token
+
+    if refresh_token:
+
+        response = post(API_URL + "/oauth/token", data= {
+        "refresh_token" : refresh_token,
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "redirect_uri": REDIRECT_URI,
+        "grant_type": "refresh_token",
+        }).json()
+
+        access_token = response.get("access_token")
+        token_type = response.get("token_type")
+        expires_in = response.get("expires_in")
+        scope = response.get("scope")
+
+
+        update_or_create_user_tokens(session_id, access_token, token_type, expires_in, refresh_token, scope)
+
 def is_trakt_auth(session_id):
     tokens = get_user_tokens(session_id)
     if tokens:
         expiry = tokens.expires_in
-        # if expiry <= timezone.now():
-            # refresh_spotify_token(session_id)
-        print(expiry)
+        if expiry <= timezone.now():
+            refresh_spotify_token(session_id)
         return True
     return False
 
@@ -24,11 +45,9 @@ def check_for_session(request):
         request.session.create()
 
 def update_or_create_user_tokens(session_id, access_token, token_type, expires_in, refresh_token, scope):
-    # print(f"\n UPDATE OR CREATE FUNCTION \n DATA: \n{session_id, access_token, token_type, expires_in, refresh_token}\n\n\n")
     tokens = get_user_tokens(session_id)
     expires_in = timezone.now() + timedelta(seconds=expires_in)
     if tokens:
-        # print("TOKENS EXIST")
         tokens.access_token = access_token
         tokens.refresh_token = refresh_token
         tokens.expires_in = expires_in
@@ -36,8 +55,8 @@ def update_or_create_user_tokens(session_id, access_token, token_type, expires_i
         tokens.scope = scope
         tokens.save(update_fields=["access_token", "refresh_token", "expires_in", "token_type", "scope"])
     else:
-        # print("TOKENS NEW")
         tokens = TraktToken(user=session_id, access_token=access_token,
                             refresh_token=refresh_token, token_type=token_type, expires_in=expires_in, scope=scope)
         tokens.save()
+
 
