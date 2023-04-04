e780c10e924933a3d863a5b1c09bf708a2c541d8
Aleksandar <coa@hotmail.com>
2023-04-04 07:41:45 +0200

    upload

--- frontend/src/pages/MediaOverview.js
+++ frontend/src/pages/MediaOverview.js
@@ -7,5 +7,34 @@ export default function MediaOverview({ title }) {
 
     console.log(data)
 
-    return <div className="movie-section">{movies}</div>
+    const movies = data.map((movie, index) => {
+        return (
+            <MovieCard
+                movieData={movie.movie}
+                watchers={movie.watchers}
+                rank={index < 2 ? "top" : "low"}
+            />
+        )
+    })
+
+    return (
+        <div className="media-overview">
+            <div className="sidebar">
+                <div>Test</div>
+                <div>Test</div>
+                <div>Test</div>
+            </div>
+            <div className="movie-section">{movies}</div>
+        </div>
+    )
+}
+
+function MovieCard({ movieData, watchers, rank }) {
+    return (
+        <div className={rank === "top" ? "top-item" : "low-item"}>
+            {/* <div>{movieData.title}</div> */}
+            {/* <div>{movieData.poster_url}</div> */}
+            {/* <div>{watchers}</div> */}
+        </div>
+    )
 }
